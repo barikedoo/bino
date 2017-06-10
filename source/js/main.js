daf679a3bcedb60d73693f283fd5cbdd93b2d898
Vyacheslav <barikedoo@gmail.com>
2017-06-10 11:38:31 +0300

    scripts added

--- source/js/main.js
+++ source/js/main.js
@@ -11,5 +11,23 @@ $(document).ready(function () {
         $(this).addClass('is-hidden');
     });
 
+    $('.recent-works__tags').on('click', '.recent-works__tags__item', function(){
+        
+        var filterType = '.recent-works-' + $(this).data('filter'),
+            elem = $('.recent-works__images__item');
+            console.log(filterType);
+            console.log(elem);
+
+            if($(this).data('filter') == 'all') {
+
+                elem.toggleClass('is-active');
+            } else {
+
+            elem.removeClass('is-active').addClass('is-blurred');
+            elem.filter(filterType).toggleClass('is-active');
+            }
+            
+    });
+
 });
 
